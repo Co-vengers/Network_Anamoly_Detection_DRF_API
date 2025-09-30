43ace0acb55e365ca4067d0c17138ae24000df8a
Co-vengers <vyomrohila@gmail.com>
2025-09-30 11:46:02 +0530

    Created: models

--- detection/models.py
+++ detection/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+import uuid
+
 # Create your models here.
+def model_upload_path(instance, filename):
+    return f"models/{instance.id}/filename"
+
+class UploadDataset(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    uploaded_by = models.ForeignKey('auth.User', null=True, on_delete=models.SET_NULL)
+    csv_file = models.FileField(upload_to='Datasets/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=32, default='uploaded')
+    meta = models.JSONField(null=True, blank=True)
+
+class ModelArtifact(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(upload_to=model_upload_path)
+    metrics = models.JSONField(null=True, blank=True)
+    params = models.JSONField(null=True, blank=True)
--- nad_api/settings.py
+++ nad_api/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest-framework',
+    'corsheaders',
+    'detection',
 ]
 
 MIDDLEWARE = [
@@ -47,7 +54,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGIN = False
+CORS_ALLOWED_ORIGIN = []    #frontend URI
 
 ROOT_URLCONF = 'nad_api.urls'
 
@@ -74,8 +85,12 @@ WSGI_APPLICATION = 'nad_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv("HOST"),
+        'PORT': os.getenv('PORT'),
     }
 }
 
