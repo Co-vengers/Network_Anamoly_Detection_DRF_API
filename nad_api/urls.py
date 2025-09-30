c9007223dafee99c198ea2598496ab3f7e2a542f
Co-vengers <vyomrohila@gmail.com>
2025-09-30 14:55:43 +0530

    Create: urls

--- nad_api/urls.py
+++ nad_api/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from detection.views import DatasetViewSet, TrainViewSet
+
+router = DefaultRouter()
+router.register(r'datasets', DatasetViewSet, basename='dataset')
+router.register(r'train', TrainViewSet, basename='train')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView, name='token_refresh'),
 ]
