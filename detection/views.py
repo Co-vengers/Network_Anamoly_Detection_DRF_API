3d664c5da82bab9b8abbcdcb236f3e09017bc7c5
Co-vengers <vyomrohila@gmail.com>
2025-09-30 14:02:49 +0530

    Updated: views

--- detection/views.py
+++ detection/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import UploadDataset, ModelArtifact
+from .serializers import UploadDatasetSerializer, ModelArtifactSerializer
 
 # Create your views here.
+class DatasetViewSet(viewsets.ModelViewSet):
+    queryset = UploadDataset.objects.all().order_by('-updated_at')
+    serializer_class = UploadDatasetSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(uploaded_by=self.request.user)
+
+class TrainViewSet(viewsets.ViewSet):
+    @action(detail=False, methods=['post'])
+    def start(self, request):
+        dataset_id = request.data.get('dataset_id')
+        params = request.data.get('params', {})
+        job = train_model_task.delay(dataset_id, params)
+        return Response({'job_id': job.id}, status=status.HTTP_202_ACCEPTED)
