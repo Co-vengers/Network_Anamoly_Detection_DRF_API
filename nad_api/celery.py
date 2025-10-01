5a4c41af2e44932aa858d517594d965333bb09ab
Co-vengers <vyomrohila@gmail.com>
2025-10-01 14:50:07 +0530

    Updates: Fixed Celery configurations and created front end

--- nad_api/celery.py
+++ nad_api/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nad_api_settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nad_api.settings')  # <-- fix here
 app = Celery('nad_api')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
